aa20201ebb638d3175625897389c8c4755aa5a6c
iarakistain <39854761+iarakistain@users.noreply.github.com>
2018-07-31 13:04:20 +0200

    Add files via upload

---
+++ mkr1000/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/iarakistain/pen/BPwwKK.
+
+ Preparing a simple dashboard to display data from the MKR1000. Using this for the Arduino MKR1000 contest for my Plant monitoring system.
---
+++ mkr1000/js/index.js
@@ -0,0 +1,204 @@
+	$(document).ready(function() {
+		var $delay = 1000,
+			vMin = 11.5,
+			vMax = 14.5,
+			cMin = .3,
+			cMax = 2.5,
+			mMin = 0,
+			mMax = 5,
+			totalPoints = 25,
+			$voltageDisplay = $('div.volts'),
+			$currentDisplay = $('div.amps'),
+			$moistureDisplay = $('div.moisture');
+
+		function getRandomInt(min, max) {
+			let reading = (Math.random() * (max - min + 1) + min);
+			return (Math.round(reading * 2) / 2)
+		}
+		
+		function updateVoltage(value) {
+			$voltageDisplay.html(value.toFixed(1));
+		}
+		
+		function updateCurrent(value) {
+			$currentDisplay.html(value.toFixed(1));
+		}
+		
+		function updateMoisture(value) {
+			$moistureDisplay.html(value.toFixed(1) + '<span>%</span>');
+		}
+		
+		function updateSensorDisplayValues(d) {
+			updateVoltage(d[0]);
+			updateCurrent(d[1]);
+			updateMoisture(d[2]);
+		}
+
+		Highcharts.setOptions({
+			global: {
+				useUTC: false
+			},
+			plotOptions: {
+				series: {
+					marker: {
+						enabled: false
+					}
+				}
+			},
+			tooltip: {
+				enabled: false
+			}
+		});
+
+		$('#sensorData').highcharts({
+			chart: {
+				type: 'spline',
+				events: {
+					load: function() {
+						var voltage = this.series[0];
+						var current = this.series[1];
+						var moisture = this.series[2];
+						var x, volts, amps, mPercent;
+
+						// faking sensor data
+						// data will be coming from sensors on the MKR1000
+						setInterval(function() {
+							x = (new Date()).getTime(),
+								volts = getRandomInt(vMin, vMax),
+								amps = getRandomInt(cMin, cMax),
+								mPercent = getRandomInt(mMin, mMax);
+							
+							voltage.addPoint([x, volts], false, true);
+							current.addPoint([x, amps], false, true);
+							moisture.addPoint([x, mPercent], true, true);
+							
+							updateSensorDisplayValues([volts, amps, mPercent]);
+						}, $delay);
+					}
+				}
+			},
+			title: {
+				text: 'Sensor Data'
+			},
+			xAxis: {
+				type: 'datetime',
+				tickPixelInterval: 500
+			},
+			yAxis: [{
+				title: {
+					text: 'VOLTAGE',
+					style: {
+						color: '#2b908f',
+						font: '13px sans-serif'
+					}
+				},
+				min: 0,
+				max: 15,
+				plotLines: [{
+					value: 0,
+					width: 1,
+					color: '#808080'
+				}]
+			}, {
+				title: {
+					text: 'CURRENT',
+					style: {
+						color: '#90ee7e',
+						font: '13px sans-serif'
+					}
+				},
+				min: 0,
+				max: 4,
+				opposite: true,
+				plotLines: [{
+					value: 0,
+					width: 1,
+					color: '#808080'
+				}]
+			}, {
+				title: {
+					text: 'MOISTURE',
+					style: {
+						color: '#f45b5b',
+						font: '13px sans-serif'
+					}
+				},
+				min: 0,
+				max: 100,
+				opposite: true,
+				plotLines: [{
+					value: 0,
+					width: 1,
+					color: '#808080'
+				}]
+			}],
+			tooltip: {
+				formatter: function() {
+					var unitOfMeasurement = this.series.name === 'VOLTAGE' ? ' V' : ' A';
+					return '<b>' + this.series.name + '</b><br/>' +
+						Highcharts.numberFormat(this.y, 1) + unitOfMeasurement;
+				}
+			},
+			legend: {
+				enabled: true
+			},
+			exporting: {
+				enabled: false
+			},
+			series: [{
+				name: 'VOLTAGE',
+				yAxis: 0,
+				style: {
+					color: '#2b908f'
+				},
+				data: (function() {
+					// generate an array of random data
+					var data = [],
+						time = (new Date()).getTime(),
+						i;
+
+					for (i = -totalPoints; i <= 0; i += 1) {
+						data.push({
+							x: time + i * $delay,
+							y: getRandomInt(12, 12)
+						});
+					}
+					return data;
+				}())
+			}, {
+				name: 'CURRENT',
+				yAxis: 1,
+				data: (function() {
+					// generate an array of random data
+					var data = [],
+						time = (new Date()).getTime(),
+						i;
+
+					for (i = -totalPoints; i <= 0; i += 1) {
+						data.push({
+							x: time + i * $delay,
+							y: getRandomInt(.7, .7)
+						});
+					}
+					return data;
+				}())
+			}, {
+				name: 'Moisture',
+				yAxis: 2,
+				data: (function() {
+					// generate an array of random data
+					var data = [],
+						time = (new Date()).getTime(),
+						i;
+
+					for (i = -totalPoints; i <= 0; i += 1) {
+						data.push({
+							x: time + i * $delay,
+							y: getRandomInt(1, 1)
+						});
+					}
+					return data;
+				}())
+			}]
+		});
+	});
