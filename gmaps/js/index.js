bba1d5b475f40869a283aaead068035581ad6bde
iarakistain <39854761+iarakistain@users.noreply.github.com>
2018-12-07 08:38:43 +0100

    Add files via upload

---
+++ gmaps/js/index.js
@@ -0,0 +1,54 @@
+//var myPolygon;
+function initialize() {
+  // Map Center
+  var myLatLng = new google.maps.LatLng(38.575764, -121.478851);
+  // General Options
+  var mapOptions = {
+    zoom: 13,
+    center: myLatLng,
+    mapTypeId: 'satellite'
+
+    //google.maps.MapTypeId.RoadMap
+  };
+  
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+  // Polygon Coordinates
+  var triangleCoords = [
+    new google.maps.LatLng(38.5762475, -121.4267386),
+    new google.maps.LatLng(38.5704882, -121.4627875),
+    new google.maps.LatLng(38.5704686, -121.4027061)
+  ];
+  // Styling & Controls
+  myPolygon = new google.maps.Polygon({
+    paths: triangleCoords,
+    draggable: true, // turn off if it gets annoying
+    editable: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35
+  });
+
+  myPolygon.setMap(map);
+  //google.maps.event.addListener(myPolygon, "dragend", getPolygonCoords);
+  google.maps.event.addListener(myPolygon.getPath(), "insert_at", getPolygonCoords);
+  //google.maps.event.addListener(myPolygon.getPath(), "remove_at", getPolygonCoords);
+  google.maps.event.addListener(myPolygon.getPath(), "set_at", getPolygonCoords);
+}
+
+//Display Coordinates below map
+function getPolygonCoords() {
+  var len = myPolygon.getPath().getLength();
+  var htmlStr = "";
+  for (var i = 0; i < len; i++) {
+    //htmlStr += "new google.maps.LatLng(" + myPolygon.getPath().getAt(i).toUrlValue(5) + "), ";
+    //Use this one instead if you want to get rid of the wrap > new google.maps.LatLng(),
+    htmlStr += "" + myPolygon.getPath().getAt(i).toUrlValue(5);
+  }
+  document.getElementById('info').innerHTML = htmlStr;
+}
+function copyToClipboard(text) {
+  window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+}
